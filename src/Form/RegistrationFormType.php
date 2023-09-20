3182aedc15bc28f4501f3e75831d4cf3d492828e
Alex <paperalex59100@hotmail.fr>
2023-09-20 16:01:26 +0200

    le mec fait un push sur la main insane

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -20,7 +21,13 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('prenom')
             ->add('nom')
-            ->add('adresse')
+            ->add('adresse', TextType::class, [
+                'required' => false,
+                'label' => 'Adresse postale',
+                'attr' => [
+                        'class' => 'autocomplete-address'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
