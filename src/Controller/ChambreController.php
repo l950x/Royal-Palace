2f75115ccef46f04193756a7ab53f8d6db80f456
L950x <loick.lp59@gmail.com>
2023-09-28 13:53:58 +0200

    chambre show fix

--- src/Controller/ChambreController.php
+++ src/Controller/ChambreController.php
@@ -28,18 +28,18 @@ class ChambreController extends AbstractController
         // $session->set('nbPersonne', null);
 
         $id = $request->attributes->get('id');
+        $chambreslibres = [];
         switch ($id) {
             case '1':
                 $chambres = $chambreRepository->findBy(['Type' => 1]);
-
                 foreach ($chambres as $chambre) {
                 $reserver = $reserverRepository->findOneBy([
-                    'chambre' => $id,
+                    'chambre' => $chambre->getId(),
                     'validite' => 0,
                 ]);
 
                 if (!$reserver) {
-                    $chambrelibres[] = $chambre;
+                    $chambreslibres[] = $chambre;
                 }
             }
 
@@ -47,9 +47,32 @@ class ChambreController extends AbstractController
 
             case '2':
                 $chambres = $chambreRepository->findBy(['Type' => 2]);
+                foreach ($chambres as $chambre) {
+                $reserver = $reserverRepository->findOneBy([
+                    'chambre' => $chambre->getId(),
+                    'validite' => 0,
+                ]);
+
+                if (!$reserver) {
+                    $chambreslibres[] = $chambre;
+                }
+            }                
+            
                 break;
+
             case '3':
                 $chambres = $chambreRepository->findBy(['Type' => 3]);
+                foreach ($chambres as $chambre) {
+                $reserver = $reserverRepository->findOneBy([
+                    'chambre' => $chambre->getId(),
+                    'validite' => 0,
+                ]);
+
+                if (!$reserver) {
+                    $chambreslibres[] = $chambre;
+                }
+            }                   
+            
                 break;
 
             default:
@@ -58,18 +81,31 @@ class ChambreController extends AbstractController
         }
 
         return $this->render('chambre/index.html.twig', [
-            'chambres' => $chambrelibres,
+            'chambres' => $chambreslibres,
         ]);
     }
 
 
     #[Route('/{id}', name: 'app_chambre_show', methods: ['GET'])]
-    public function show(Chambre $chambre, Session $session): Response
+    public function show(ReserverRepository $reserverRepository,Chambre $chambre, Session $session): Response
     {
         $session->set('preChambre', $chambre->getId());
+        $nonDisponible = false;
+        
+        $reserver = $reserverRepository->findOneBy([
+            'chambre' => $chambre->getId(),
+            'validite' => 0,
+        ]);
+
+        if ($reserver) {
+            $nonDisponible = true;
+        }
+
+
         return $this->render('chambre/show.html.twig', [
             'chambre' => $chambre,
             'numChambre' => $chambre->getId(),
+            'nonDisponible' => $nonDisponible,
         ]);
     }
 
